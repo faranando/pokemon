5ffb2d124876e4fc8eb7dc1285c15c29fe8119d1
faranando <ff.felixfaranando@gmail.com>
2022-10-15 06:06:45 +0700

    update Route path for OwnedPokemon element

--- src/App.js
+++ src/App.js
@@ -96,7 +96,7 @@ export default function App(){
     <Routes>
       <Route path="/pokemon-react" element={<Home pokemonData={pokemonData} loading={loading} setUrl={setUrl} nextUrl={nextUrl} />} />
       <Route path="/pokemon-react/pokemon/:id" element={<Pokemon pokemonData={pokemonData} ownedPokemonData={ownedPokemonData} changePokemonData={changePokemonData} changeOwnedPokemonData={changeOwnedPokemonData} />} />
-      <Route path="/bag" element={<OwnedPokemon pokemonData={ownedPokemonData} loading={loading} />}  />
+      <Route path="/pokemon-react/bag" element={<OwnedPokemon pokemonData={ownedPokemonData} loading={loading} />}  />
     </Routes>
     <Footer />
   </div>
